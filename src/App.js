cf6ed79e1bb6120ac44c044874722ce30172fd31
freda <pseuyi@gmail.com>
2024-06-23 19:15:50 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,174 @@
+import Tangrams from "./Tangrams";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="title">
+        <b>freda suyi ding</b>
+      </header>
+      <div className="topLinks">
+        <div className="softButton">
+          <a href="https://github.com/pseuyi" target="_blank" rel="noreferrer">
+            github
+          </a>
+        </div>
+        <div className="softButton">
+          <a href="https://gitlab.com/pseuyi" target="_blank" rel="noreferrer">
+            gitlab
+          </a>
+        </div>
+        <div className="softButton">
+          <a
+            href="https://www.linkedin.com/in/pseuyi/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            linkedin
+          </a>
+        </div>
+        <div className="softButton">
+          <a
+            href="https://docs.google.com/document/d/18GQSqlOH4n46326_tAB26Go9NKEBiMQGqiRomDJTP6w/edit?usp=sharing"
+            rel="noreferrer"
+          >
+            resume
+          </a>
+        </div>
+      </div>
+
+      <div className="sections">
+        <details open>
+          <summary className="summary">currently</summary>
+          <div className="info">
+            i'm working on{" "}
+            <a
+              href="https://www.costarastrology.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="clickable"
+            >
+              ai-powered creative content generation
+            </a>{" "}
+            using functional programming paradigms.
+          </div>
+        </details>
+
+        <details>
+          <summary className="summary">about me</summary>
+          <div className="info">
+            i'm a computer programmer based in chinatown, nyc interested in
+            writing elegant, safe, and functional software.
+            <br />
+            outside of programming i can be found{" "}
+            <a
+              href="https://www.mountainproject.com/user/200352257/freda-nyc"
+              target="_blank"
+              rel="noreferrer"
+              className="clickable"
+            >
+              climbing
+            </a>{" "}
+            or
+            <a
+              href="https://art.pseuyi.dev"
+              target="_blank"
+              rel="noreferrer"
+              className="clickable"
+            >
+              {" "}
+              drawing
+            </a>
+            .<p>links</p>
+            <ul>
+              <li>
+                <a
+                  href="https://www.are.na/after-freda"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  are.na
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://special.fish/after_freda"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  𓆝 𓆟 𓆞
+                </a>
+              </li>
+              <li>
+                <a
+                  href="mailto:pseuyi@gmail.com?Subject=hi"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  send me mail
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://webring.recurse.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    alt="recurse center logo"
+                    src="https://webring.recurse.com/icon.png"
+                    width={16}
+                    style={{ transform: "translate(0, 2px)" }}
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </details>
+
+        <details>
+          <summary className="summary">site archive</summary>
+          <div className="info">
+            some former iterations
+            <ul>
+              <li>
+                <a
+                  href="https://pseuyi.github.io/2017/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  2017
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://pseuyi.github.io/2016/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  2016
+                </a>
+              </li>
+            </ul>
+          </div>
+        </details>
+      </div>
+
+      <Tangrams />
+
+      <footer>
+        <div className="guestbookButton">
+          <a
+            href="http://pseuyi.123Guestbook.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="guestbook">📖</span> guestbook
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
